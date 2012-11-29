70ebd2e89614c718cf06cf3831fea5fc9a5c3ff6
Sebastian Niciarz <sebastian.niciarz@intel.com>
2012-11-29 22:16:14 +0200

    [PORT FROM MAIN] Stk: Integrate support for class "m"
    BZ: 11536
    
    DO NOT UPSTREAM
    
    Implement forwarding of the HCI CONNECTIVITY
    events from CLF/NFC to the UICC through
    Event Download.
    
    Change-Id: Iae8a1a7b3307fd06abb6511724803ca73b932fa8
    Origin-Change-Id: Id51efa4b65236aaba818f8f07894ce79c66def51
    Signed-off-by: Sebastian Niciarz <sebastian.niciarz@intel.com>
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
    Reviewed-on: http://android.intel.com:8080/77816
    Reviewed-by: Amegah, AndreX <andrex.amegah@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

---
+++ src/com/android/stk/NfcBroadcastReceiver.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.stk;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.android.internal.telephony.cat.AppInterface;
+import com.android.internal.telephony.cat.CatEventMessage;
+import com.android.internal.telephony.cat.CatLog;
+import com.android.internal.telephony.cat.EventCode;
+
+public class NfcBroadcastReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+        if ("com.nxp.action.CONNECTIVITY_EVENT_DETECTED".equals(intent.getAction())) {
+            CatLog.d(this, "NFC --> UICC: HCI Connectivity event detected" );
+            AppInterface catService = com.android.internal.telephony.cat.CatService.getInstance();
+            if (catService != null) {
+                catService.onEventDownload(
+                        new CatEventMessage(EventCode.HCI_CONNECTIVITY_EVENT.value(), null, true));
+                CatLog.d(this, "Event Download - HCI Connectivty sent to the UICC" );
+            } else {
+                CatLog.d(this, "Error: CatService getInstance returned null" );
+            }
+        }
+    }
+}
